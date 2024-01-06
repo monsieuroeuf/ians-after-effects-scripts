f9a493836ada6f8e54e9e3c48848461ed750df0d
Ian Haigh <ian@ketchup.net.au>
2024-01-06 12:15:04 +1100

    added comps to same time

---
+++ src/All Comps to Same Time.ts
@@ -0,0 +1,26 @@
+﻿//@target aftereffects
+
+/**
+ * Sets the current time in all selected comps to 4 seconds. So I can see titles
+ * after they've built, and ensure everything looks OK.
+ * 
+ * @todo make it work with more selections, e.g. loose comps or more than one folder
+ * @todo hold down a key to set a default for the time
+ */
+
+(function allCompsToSameTime() {
+	//@include "../lib/aequery.js"
+	var sel = app.project.activeItem;
+	var compsToChange;
+
+	if (sel && sel.typeName === "Folder") {
+		compsToChange = aeq.getComps(sel);
+	} else {
+		compsToChange = aeq.project.getSelectedComps();
+	}
+
+	aeq.forEach(compsToChange, function (comp) {
+		comp.time = 4;
+		comp.openInViewer();
+	});
+})();
