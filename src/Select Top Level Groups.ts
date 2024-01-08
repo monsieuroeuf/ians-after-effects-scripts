6bea78636ee3a634ae781bc747d907f5a526f280
Ian Haigh <ian@ketchup.net.au>
2024-01-08 18:00:38 +1100

    Update Select Top Level Groups.ts

--- src/Select Top Level Groups.ts
+++ src/Select Top Level Groups.ts
@@ -10,7 +10,6 @@
  *  just one. Weird
  */
 
-
 (function selectTopLevelGroups() {
 	//@include "./lib/aequery.js"
 	clearOutput()
@@ -27,7 +26,9 @@
 			continue
 		}
 
-		const numProperties = currentLayer.property("Contents").numProperties
+		const contentsProp = currentLayer.property("Contents") as PropertyGroup
+		const numProperties = contentsProp.numProperties
+
 		if (numProperties === 0) continue
 
 		for (let i = 1; i <= numProperties; i++) {
