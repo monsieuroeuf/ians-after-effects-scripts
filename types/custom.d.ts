fe9799ce69e04efda4de6c453bf8d8cc0621c44c
Ian Haigh <ian@ketchup.net.au>
2024-03-15 11:31:13 +1100

    housekeeping

---
+++ types/custom.d.ts
@@ -0,0 +1,4 @@
+interface Array<T> {
+  map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[];
+  forEach(callbackfn: (value: T, index: number, array: T[]) => void, thisArg?: any): void;
+}
