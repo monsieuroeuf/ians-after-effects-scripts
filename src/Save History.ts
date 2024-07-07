9195959ab7638a7449b451462b4be1e2988eabfd
Ian Haigh <ian@ketchup.net.au>
2024-07-07 14:56:15 +1000

    save history – work-in-progress

---
+++ src/Save History.ts
@@ -0,0 +1,24 @@
+//@target aftereffects
+
+
+
+(function saveHistory() {
+
+	// app.beginUndoGroup("Quick Comp Prefixer")
+	// const KEY_NAME = "quickCompPrefixer"
+
+	let proj = app.project.activeItem
+	writeLn(proj.name)
+
+	// look 
+
+
+	// let defaultString = IanLib.getPref(KEY_NAME)
+
+	// let s = Window.prompt("Prefix?", defaultString)
+	// IanLib.setPref(KEY_NAME, s)
+
+	// for (let currentComp of selectedComps) {
+	// currentComp.name = `${s}${currentComp.name}`
+	// }
+})()
