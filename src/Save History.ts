3f93c65d32b34fca062dfd77f1b9d293c906c1f3
Ian Haigh <ian@ketchup.net.au>
2024-07-26 15:44:45 +1000

    chore: Refactor Save History.ts for better versioning and project management

--- src/Save History.ts
+++ src/Save History.ts
@@ -1,14 +1,36 @@
 //@target aftereffects
 
 
-
 (function saveHistory() {
-	function pad(str: string, targetLength: number, padChar: string = '0'): string {
-		while (str.length < targetLength) {
-			str = padChar + str
-		}
-		return str
+	clearOutput()
+
+	const proj = app.project
+	const versionsFolder = getVersionsFolder()
+
+	const projName: string = proj.file.name
+	const projBasename: string = projName.split(".")[0]
+
+	
+	if (workingOnTheWrongFile()) {
+		alert("You are working on a version of the project. Please open the master project.")
+		return
 	}
+
+	proj.save()
+	// keep track of this one, since we will need to re-open it
+	const originalPath = proj.file.fullName
+	
+	const highestVersion = getNextVersionNumber()
+
+	// save the current project as a version
+	let numberFormatted = pad(highestVersion.toString(), 2, "0")
+	let newName = `${projBasename}-h${numberFormatted}.aep`
+	let newPath = File(versionsFolder.fullName + "/" + newName)
+
+	// I can't figure out how to get to "Save a copy"
+	app.project.save(newPath)
+	app.open(File(originalPath))
+
 
 	function getVersionsFolder() {
 		const VERSIONS_FOLDER = "support/v_versions/"
@@ -25,9 +47,11 @@
 	}
 
 	function workingOnTheWrongFile() {
-		if (projBasename.indexOf("-h") !== -1) {
+		const regexp = /-h\d\d\.aep$/
+		if (projBasename.match(regexp)) {
 			return true
 		}
+		return false
 	}
 
 	function getNextVersionNumber() {
@@ -37,80 +61,37 @@
 		let candidates = []
 		for (let version of versions) {
 			// does the version match the current project? 
-			// writeLn(version.name.indexOf(projBasename).toString())
 			if (version.name.indexOf(projBasename) !== -1) {
 				candidates.push(version)
 			}
-			// writeLn(version.name)
 		}
 
 		// what is the highest version number?
-		let currentHigh = 0
+		let keepTrackOfHighest = 0
 		let versionNumber = 0 // Default to 0 or any appropriate default value
 
 		for (let candidate of candidates) {
 			// use a regexp to match the version number
-			const matchResult = candidate.name.match(/-h(\d+)/)
+			const matchResult = candidate.name.match(/-h(\d\d)\.aep/)
 
 			if (matchResult) {
 				versionNumber = parseInt(matchResult[1], 10) // Use the first captured group
 			}
 
-			writeLn(parseInt(matchResult[1]).toString())
-
-
-			// let versionNumber = parseInt(candidate.name.split("-h")[1])
-			if (versionNumber > currentHigh) {
-				currentHigh = versionNumber
+			if (versionNumber > keepTrackOfHighest) {
+				keepTrackOfHighest = versionNumber
 			}
 		}
 
-		writeLn("it is " + currentHigh.toString())
-		return currentHigh + 1
+		return keepTrackOfHighest + 1
 
 	}
 
-
-	clearOutput()
-
-
-	const proj = app.project
-	const versionsFolder = getVersionsFolder()
-
-	const projName: string = proj.file.name
-	const projBasename: string = projName.split(".")[0]
-
-
-	if (workingOnTheWrongFile()) {
-		alert("You are working on a version of the project. Please open the master project.")
-		return
+	function pad(n: string, width: number, z: string) {
+		z = z || '0'
+		n = n + ''
+		return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
 	}
 
-	const highestVersion = getNextVersionNumber()
 
-
-	// save the current project as a version
-	let numberFormatted = pad(highestVersion.toString() + 1, 2, "0")
-	let neName = `${projBasename}-h${numberFormatted}.aep`
-	let newPath = File(versionsFolder.fullName + "/" + neName)
-
-	writeLn(newPath.fullName)
-	// writeLn(newVersionName)
-	// alert(newVersionPath.path)
-
-	// app.project.save(newVersionPath)
-
-
-
-	// look 
-
-
-	// let defaultString = IanLib.getPref(KEY_NAME)
-
-	// let s = Window.prompt("Prefix?", defaultString)
-	// IanLib.setPref(KEY_NAME, s)
-
-	// for (let currentComp of selectedComps) {
-	// currentComp.name = `${s}${currentComp.name}`
-	// }
 })()
