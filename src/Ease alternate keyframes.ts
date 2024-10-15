20d811709b4b91c54de17f18273843d644ac8620
Ian Haigh <ian@ketchup.net.au>
2024-10-16 09:22:02 +1100

    Refactor keyframe easing and add expression control

--- src/Ease alternate keyframes.ts
+++ src/Ease alternate keyframes.ts
@@ -13,14 +13,11 @@
 			case PropertyValueType.ThreeD_SPATIAL:
 			case PropertyValueType.ThreeD:
 				return 3
-				break
 			case PropertyValueType.TwoD_SPATIAL:
 			case PropertyValueType.TwoD:
 				return 2
-				break
 			case PropertyValueType.OneD:
 				return 1
-				break
 			default:
 				return 0
 		}
@@ -32,13 +29,13 @@
 
 		let easeIn: KeyframeEase
 		let easeOut: KeyframeEase
-		var counter = 0 // this needs to be kept separate from the loop counter, so we can skip roving keyframes
+		// let counter = 0 // this needs to be kept separate from the loop counter, so we can skip roving keyframes
 
-		var smooth = new KeyframeEase(0.1, 60)
-		var sharp = new KeyframeEase(0.0, 0.1)
+		const smooth = new KeyframeEase(0.1, 60)
+		const sharp = new KeyframeEase(0.0, 0.1)
 
-		for (var c = 1; c <= prop.numKeys; c++) {
-			if (c % 2 == 0) {
+		for (let c = 1; c <= prop.numKeys; c++) {
+			if (c % 2 === 0) {
 				easeIn = smooth
 				easeOut = sharp
 			} else {
@@ -51,9 +48,9 @@
 
 	app.beginUndoGroup("Ease alternate keyframes")
 
-	var selectedProperties = aeq.getSelectedProperties()
+	const selectedProperties = aeq.getSelectedProperties()
 
-	for (let current of selectedProperties) {
+	for (const current of selectedProperties) {
 		if ('propertyValueType' in current) {
 			easeEmKeys(current)
 		}
---
+++ src/addExpressionControl.ts
@@ -0,0 +1,16 @@
+﻿//@target aftereffects
+
+(function addExpressionControl() {
+	app.beginUndoGroup("Add Expression Control")
+
+	const comp = app.project.activeItem as CompItem
+	if (!comp) { return }
+
+	const selLayers = comp.selectedLayers
+
+	for (let i=1; i<selLayers.length; i++) {
+		const currentLayer = selLayers[i]
+		currentLayer.addProperty
+	}
+
+})()
