14a1cd1095f21357c77c2028a098f4f58b53bbaa
Ian Haigh <ian@ketchup.net.au>
2024-10-31 13:52:58 +1100

    Move openAllWithLabel.ts to comps and add selectAllWithLabel.ts for selecting labeled compositions

---
+++ src/comps/openAllWithLabel.ts
@@ -0,0 +1,15 @@
+//@target aftereffects
+
+(function openAllWithLabel() {
+    //@include "../lib/aequery.js"
+    app.beginUndoGroup("Open all with label")
+
+    clearOutput()
+    const greenComps = aeq('comp[label=10]') as AEQArrayEx<CompItem>
+
+    greenComps.forEach((comp: CompItem) => {
+        writeLn(comp.name)
+        comp.openInViewer()
+    })
+
+})()
--- src/comps/selectAllWithLabel.ts
+++ src/comps/selectAllWithLabel.ts
@@ -1,15 +1,15 @@
 //@target aftereffects
 
 (function openAllWithLabel() {
-    //@include "./lib/aequery.js"
-    app.beginUndoGroup("Open all with label")
+    //@include "../lib/aequery.js"
+    app.beginUndoGroup("Select all with label")
 
     clearOutput()
     const greenComps = aeq('comp[label=10]') as AEQArrayEx<CompItem>
 
     greenComps.forEach((comp: CompItem) => {
-        writeLn(comp.name)
-        comp.openInViewer()
+        // writeLn(comp.name)
+        comp.selected = true
     })
 
 })()
