341e11b46260d576d9140afcc91ffcc1ad93961c
Ian Haigh <ian@ketchup.net.au>
2024-11-08 07:37:34 +1100

    Enhance render folder script to skip folders starting with an underscore and improve logging

--- src/rendering/renderFolder.ts
+++ src/rendering/renderFolder.ts
@@ -2,25 +2,35 @@
     const proj = app.project
     const selectedItems = proj.selection
 
+    app.beginUndoGroup('Render Folder')
+
     for (let i = 0; i < selectedItems.length; i++) {
         const item = selectedItems[i]
-        writeLn(`Adding ${item.name} to render queue`);
+        writeLn(`Adding ${item.name} to render queue`)
         if (item instanceof FolderItem) {
             addComps(item)
         }
     }
-})()
-
-function addComps(folder: FolderItem) {
-    const items = folder.items
-    writeLn(`Adding comps from ${folder.name}`);
-    for (let i = 1; i <= items.length; i++) {
-        const item = items[i]
-        if (item instanceof CompItem) {
-            app.project.renderQueue.items.add(item)
-        } else if (item instanceof FolderItem) {
-            addComps(item)
+    function addComps(folder: FolderItem) {
+        // skip folders that begin with an underscore
+        if (folder.name.charAt(0) === '_') {
+            writeLn(`Skipping ${folder.name}`)
+            return
         }
+
+        const items = folder.items
+        writeLn(`Adding comps from ${folder.name}`)
+        for (let i = 1; i <= items.length; i++) {
+            const item = items[i]
+            if (item instanceof CompItem) {
+                app.project.renderQueue.items.add(item)
+            } else if (item instanceof FolderItem) {
+                addComps(item)
+            }
+        }
+
     }
 
-}
+    app.endUndoGroup()
+})()
+
